be10a7a27c3df4209f5afc6de477099ffbc6f520
Zahid Rabbi <alzahidrabbi@gmail.com>
2021-06-17 12:44:14 +0600

    pagination completed

--- src/components/RowView/RowView.js
+++ src/components/RowView/RowView.js
@@ -1,11 +1,74 @@
-import React from "react";
-import "./RowView.css"
+import React, { useEffect } from "react";
+import "./RowView.css";
 import SideBar from "../SideBar/SideBar";
 import { Avatar, IconButton } from "@material-ui/core";
-
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import { useState } from "react";
+import ReactPaginate from 'react-paginate';
+
+import TextTruncate from "react-text-truncate";
 
 const RowView = () => {
+  var TextTruncate = require("react-text-truncate");
+
+  const [news, setNews] = useState([]);
+
+  const firstTen = news.slice(0, 10);
+
+  console.log(firstTen);
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersPerPage = 3;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const displayNews = news
+    .slice(pagesVisited, pagesVisited + usersPerPage).map((singleNews) => (
+      <div className="row_view" style={{ width: "100%" }}>
+        <Avatar
+          src={
+            singleNews.image ||
+            "https://www.first.org/blog/_media/board_SF_2018.jpg"
+          }
+        />
+        <div class="card-body">
+          <h5 class="card-title">
+            <TextTruncate
+              line={1}
+              element="span"
+              truncateText="…"
+              text={singleNews.title}
+              textTruncateChild={<a href="#"></a>}
+            />
+          </h5>
+
+          <TextTruncate
+            line={1}
+            element="span"
+            truncateText="…"
+            text={singleNews.summary}
+            textTruncateChild={<a href="#"></a>}
+          />
+          <small>{singleNews.published}</small>
+        </div>
+        <IconButton>
+          <DeleteForeverIcon color="secondary" />
+        </IconButton>
+      </div>
+    ));
+
+  useEffect(() => {
+    fetch(`https://api.first.org/data/v1/news`)
+      .then((res) => res.json())
+      .then((data) => setNews(data.data));
+  }, []);
+
+  const pageCount = Math.ceil(news.length / usersPerPage);
+
+  const changePage = ({selected}) => {
+    setPageNumber(selected);
+  }
+
   return (
     <div className="row mt-5">
       <div className="col-3">
@@ -13,19 +76,18 @@ const RowView = () => {
       </div>
 
       <div className="col-9">
-        <div className="row_view" style={{ width: "100%" }}>         
-          <Avatar />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text dddd to build on the card title and make
-              up the bulk of the card's content.
-            </p>            
-          </div>
-          <IconButton>
-            <DeleteForeverIcon color="secondary" />
-          </IconButton>
-        </div>
+        {displayNews}
+        <ReactPaginate 
+          previousLabel = {"previous"}
+          nextLabel = {"Next"}
+          pageCount = {pageCount}
+          onPageChange = {changePage}
+          containerClassName={"paginationBtn"}
+          previousLinkClassName={"previousBtn"}
+          nextLinkClassName={"nextBtn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
+        />
       </div>
     </div>
   );
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -3,7 +3,9 @@ import React from 'react';
 const SideBar = () => {
     return (
         <div>
-            <h3>this is side bar...</h3>
+            <h3>Hi Reader,</h3>
+            <h3>Toggle</h3>
+            <h3>Feedback</h3>
         </div>
     );
 };
